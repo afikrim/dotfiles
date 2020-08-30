0e573bd87c170ce3cdfca782c35aac442fc2ed94
Aziz Fikri <afikrim10@student.ub.ac.id>
2020-08-31 06:26:32 +0700

    Add remove dotfile

--- backupDotfiles.sh
+++ backupDotfiles.sh
@@ -58,3 +58,28 @@ function restore-dotfile() {
     echo "Create symlink $dotname to $HOME/.$dotfilePath[-1]"
     ln -sf $dotname $HOME/.$dotfilePath[-1]
 }
+
+function remove-dotfile() {
+    if ! [ -z "$1" ]
+    then
+        dotname=$PWD/$1
+        if ! [ -f "$dotname" ]
+        then
+            if ! [ -d "$dotname" ]
+            then
+                echo "$dotname not exist"
+                return
+            fi
+        fi
+    else
+        echo "Please add dotfile in your backup directory"
+        return
+    fi
+
+    dotfilePath=($(echo $dotname | tr "/" "\n"))
+
+    echo "Remove symlink for $HOME/.$dotfilePath[-1]"
+    unlink $HOME/.$dotfilePath[-1]
+    echo "Move $dotname to $HOME/.$dotfilePath[-1]"
+    mv $dotname $HOME/.$dotfilePath[-1]
+}
