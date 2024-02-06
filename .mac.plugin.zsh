e4352c59052d784f016596d89ebeac19c5a7653f
Aziz Fikri <afikrim10@gmail.com>
2024-02-06 10:44:59 +0700

    feat: update submodules

--- .mac.plugin.zsh
+++ .mac.plugin.zsh
@@ -5,7 +5,7 @@ export PATH="$PNPM_HOME:$PATH"
 
 export LOCAL_BIN="$HOME/.local/bin"
 export PATH="$LOCAL_BIN:$PATH"
-export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
+export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="$PATH:$(go env GOPATH)/bin"
 
 source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
@@ -20,3 +20,7 @@ if [ -d "${PHPENV_ROOT}" ]; then
   export PATH="${PHPENV_ROOT}/bin:${PATH}"
   eval "$(phpenv init -)"
 fi
+
+# Get java home from libexec
+export JAVA_HOME="$(/usr/libexec/java_home -v 17)"
+export PATH="$JAVA_HOME/bin:$PATH"
