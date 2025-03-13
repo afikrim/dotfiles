604d0da28b12b7399ebd081672906905145f90bd
Aziz Fikri Mahmudi <afikrim10@gmail.com>
2025-03-13 10:43:29 +0700

    backup

--- .mac.plugin.zsh
+++ .mac.plugin.zsh
@@ -35,3 +35,5 @@ export CPPFLAGS="-I/opt/homebrew/opt/mysql-client/include"
 
 export PKG_CONFIG_PATH="/opt/homebrew/opt/mysql-client/lib/pkgconfig"
 export PATH="/opt/homebrew/bin:$PATH"
+
+source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme
